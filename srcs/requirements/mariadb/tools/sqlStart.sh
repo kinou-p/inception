2014a71dbaa6d7b5fc37d0a652c18ac103960800
kinou-p <alexpomms@gmail.com>
2022-10-09 11:25:04 +0200

    really done?

--- srcs/requirements/mariadb/tools/sqlStart.sh
+++ srcs/requirements/mariadb/tools/sqlStart.sh
@@ -5,9 +5,15 @@ service mysql start
 if [ ! -d /var/lib/mysql/wordpress ]
 then
 echo "----sql config----"
-mysql < db.sh
-#mysql -u root < pwd.sh
 
+mysql << EOF
+CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE};
+CREATE USER IF NOT EXISTS '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';
+GRANT ALL PRIVILEGES ON *.* TO '${MARIADB_USER}'@'%';
+FLUSH PRIVILEGES;
+EOF
+mysql -u root < pwd.sh
+echo "----sql config done----"
 fi
 service mysql stop
 mysqld
